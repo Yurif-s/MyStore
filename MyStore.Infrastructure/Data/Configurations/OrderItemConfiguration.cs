cfa174a464a6f07bdcecd4c9e830818494fe7c61
Yuri <yurimfs30@gmail.com>
2026-06-04 14:06:35 -0300

    fix(infrastructure): add missing OrderItemConfiguration

---
+++ MyStore.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyStore.Domain.Entities;
+
+namespace MyStore.Infrastructure.Data.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.UnitPrice)
+            .IsRequired()
+            .HasPrecision(18, 2);
+
+        builder.Property(i => i.Quantity)
+            .IsRequired();
+
+        builder.Ignore(i => i.Subtotal);
+    }
+}
