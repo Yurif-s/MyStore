b3be8a8330f0d55d2b8ec82cb6ea86a56a062326
Yuri <yurimfs30@gmail.com>
2026-06-10 13:30:04 -0300

    feat(infrastructure): add cart repository

---
+++ MyStore.Infrastructure/Repositories/CartRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyStore.Domain.Entities;
+using MyStore.Domain.Interfaces;
+using MyStore.Infrastructure.Data;
+
+namespace MyStore.Infrastructure.Repositories;
+
+internal class CartRepository(AppDbContext dbContext)
+    : Repository<Cart>(dbContext), ICartRepository
+{
+    public async Task<Cart?> GetByUserIdAsync(string userId, CancellationToken ct = default)
+        => await _dbContext.Carts
+            .Include(c => c.Items)
+            .FirstOrDefaultAsync(c => c.UserId == userId, ct);
+}
