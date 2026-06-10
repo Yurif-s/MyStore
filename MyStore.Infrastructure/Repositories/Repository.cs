c4d4ba5188dca6645fe0bf37fbef13f3dc167ea0
Yuri <yurimfs30@gmail.com>
2026-06-10 13:25:09 -0300

    feat(infrastructure): add generic repository

---
+++ MyStore.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyStore.Domain.Interfaces;
+using MyStore.Infrastructure.Data;
+
+namespace MyStore.Infrastructure.Repositories;
+
+internal class Repository<T>(AppDbContext dbContext) : IRepository<T> where T : class
+{
+    protected readonly AppDbContext _dbContext = dbContext;
+
+    public async Task AddAsync(T entity, CancellationToken ct = default) 
+        => await _dbContext.Set<T>().AddAsync(entity, ct);
+
+    public async Task<IEnumerable<T>> GetAllAsync(CancellationToken ct = default)
+        => await _dbContext.Set<T>().AsNoTracking().ToListAsync(ct);
+
+    public async Task<T?> GetByIdAsync(int id, CancellationToken ct = default)
+        => await _dbContext.Set<T>().FindAsync([id], ct);
+
+    public void Remove(T entity)
+        => _dbContext.Set<T>().Remove(entity);
+
+    public void Update(T entity)
+        => _dbContext.Set<T>().Update(entity);
+}
