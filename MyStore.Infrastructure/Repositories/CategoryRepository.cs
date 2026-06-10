cb9d15bbb0a81a313ba846b3115b8a0fa9590a68
Yuri <yurimfs30@gmail.com>
2026-06-10 13:28:39 -0300

    feat(infrastructure): add category repository

---
+++ MyStore.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyStore.Domain.Entities;
+using MyStore.Domain.Interfaces;
+using MyStore.Infrastructure.Data;
+
+namespace MyStore.Infrastructure.Repositories;
+
+internal class CategoryRepository(AppDbContext dbContext)
+    : Repository<Category>(dbContext), ICategoryRepository
+{
+    public async Task<Category?> GetBySlugAsync(string slug, CancellationToken ct = default)
+        => await _dbContext.Categories
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Slug == slug, ct);
+}
