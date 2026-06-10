734658ea48826e41a8737ea388c7a7c1c43a4c2e
Yuri <yurimfs30@gmail.com>
2026-06-10 13:30:48 -0300

    feat(infrastructure): add unit of work

---
+++ MyStore.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using MyStore.Domain.Interfaces;
+using MyStore.Infrastructure.Data;
+
+namespace MyStore.Infrastructure.Repositories;
+
+internal class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public async Task CommitAsync(CancellationToken ct = default)
+        => await context.SaveChangesAsync(ct);
+}
