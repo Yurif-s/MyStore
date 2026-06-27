60697a118e0181f75896821075c5f5b79a506134
Yuri <yurimfs30@gmail.com>
2026-06-27 15:05:19 -0300

    feat(application): add culture middleware

---
+++ MyStore.Web/Middlewares/CultureMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace MyStore.Web.Middlewares;
+
+public class CultureMiddleware(RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("en");
+
+        if (!string.IsNullOrWhiteSpace(requestedCulture)
+            && supportedLanguages.Exists(l => l.Name.Equals(requestedCulture)))
+            cultureInfo = new CultureInfo(requestedCulture);
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await next(context);
+    }
+}
