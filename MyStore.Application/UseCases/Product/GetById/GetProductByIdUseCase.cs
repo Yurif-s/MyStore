9631b7a99c65a3989053eadd3f94775212f8dffd
Yuri <yurimfs30@gmail.com>
2026-06-27 15:13:20 -0300

    feat(application): add use case for get product by id

---
+++ MyStore.Application/UseCases/Product/GetById/GetProductByIdUseCase.cs
@@ -0,0 +1,34 @@
+﻿using MyStore.Application.DTOs.Product;
+using MyStore.Application.Messages;
+using MyStore.Domain.Interfaces;
+
+namespace MyStore.Application.UseCases.Product.GetById;
+
+public class GetProductByIdUseCase(
+    IProductRepository productRepository,
+    ICategoryRepository categoryRepository) : IGetProductByIdUseCase
+{
+    public async Task<Result<ProductDto>> ExecuteAsync(int id, CancellationToken ct = default)
+    {
+        var product = await productRepository.GetByIdAsync(id, ct);
+
+        if (product is null)
+            return Result<ProductDto>.NotFound(ResourceErrorMessages.PRODUCT_NOT_FOUND);
+
+        var category = await categoryRepository.GetByIdAsync(product.CategoryId, ct);
+        if (category is null)
+            return Result<ProductDto>.NotFound(ResourceErrorMessages.CATEGORY_NOT_FOUND);
+
+        var response = new ProductDto(
+            product.Id,
+            product.Name,
+            product.Description,
+            product.Price,
+            product.Stock,
+            product.IsActive,
+            product.CategoryId,
+            category.Name);
+
+        return Result<ProductDto>.Success(response);
+    }
+}
---
+++ MyStore.Application/UseCases/Product/GetById/IGetProductByIdUseCase.cs
@@ -0,0 +1,9 @@
+﻿using MyStore.Application.DTOs.Product;
+using MyStore.Application.Messages;
+
+namespace MyStore.Application.UseCases.Product.GetById;
+
+public interface IGetProductByIdUseCase
+{
+    public Task<Result<ProductDto>> ExecuteAsync(int id, CancellationToken ct = default);
+}
