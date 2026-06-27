2c0266f833729299d6d83c0c232c0a8e4677c5d3
Yuri <yurimfs30@gmail.com>
2026-06-27 15:12:48 -0300

    feat(application): add use case for get all products

---
+++ MyStore.Application/UseCases/Product/GetAll/GetAllProductsUseCase.cs
@@ -0,0 +1,36 @@
+﻿using MyStore.Application.DTOs.Product;
+using MyStore.Application.Messages;
+using MyStore.Domain.Interfaces;
+
+namespace MyStore.Application.UseCases.Product.GetAll;
+
+public class GetAllProductsUseCase(
+    IProductRepository productRepository,
+    ICategoryRepository categoryRepository) : IGetAllProductsUseCase
+{
+    public async Task<Result<List<ProductDto>>> ExecuteAsync(CancellationToken ct = default)
+    {
+        var products = await productRepository.GetAllAsync();
+
+        if (!products.Any())
+            return Result<List<ProductDto>>.NoContent();
+
+        var categoryIds = products.Select(p => p.CategoryId).Distinct().ToList();
+
+        var categories = await categoryRepository.GetByIdsAsync(categoryIds, ct);
+        var categoryMap = categories.ToDictionary(c => c.Id, c => c.Name);
+
+        var response = products.Select(p => new ProductDto(
+            p.Id,
+            p.Name,
+            p.Description,
+            p.Price,
+            p.Stock,
+            p.IsActive,
+            p.CategoryId,
+            categoryMap.GetValueOrDefault(p.CategoryId, string.Empty)
+        )).ToList();
+
+        return Result<List<ProductDto>>.Success(response);
+    }
+}
---
+++ MyStore.Application/UseCases/Product/GetAll/IGetAllProductsUseCase.cs
@@ -0,0 +1,9 @@
+﻿using MyStore.Application.DTOs.Product;
+using MyStore.Application.Messages;
+
+namespace MyStore.Application.UseCases.Product.GetAll;
+
+public interface IGetAllProductsUseCase
+{
+    public Task<Result<List<ProductDto>>> ExecuteAsync(CancellationToken ct = default);
+}
