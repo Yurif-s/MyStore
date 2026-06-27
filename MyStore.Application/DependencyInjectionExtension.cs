b10e594e59c72d2537f2f652481ebecd116ea5d6
Yuri <yurimfs30@gmail.com>
2026-06-27 15:14:15 -0300

    feat(application): register usecases in DI

---
+++ MyStore.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyStore.Application.UseCases.Product.Create;
+using MyStore.Application.UseCases.Product.GetAll;
+using MyStore.Application.UseCases.Product.GetById;
+
+namespace MyStore.Application;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddUseCases(services);
+    }
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<ICreateProductUseCase, CreateProductUseCase>();
+        services.AddScoped<IGetAllProductsUseCase, GetAllProductsUseCase>();
+        services.AddScoped<IGetProductByIdUseCase, GetProductByIdUseCase>();
+    }
+}
