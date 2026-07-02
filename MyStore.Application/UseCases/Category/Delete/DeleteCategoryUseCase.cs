d2dd68a9256b314fab5694257231819b740d50c6
Yuri <yurimfs30@gmail.com>
2026-07-02 11:21:24 -0300

    feat(application): add use case for delete category

---
+++ MyStore.Application/UseCases/Category/Delete/DeleteCategoryUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MyStore.Application.Messages;
+using MyStore.Domain.Interfaces;
+
+namespace MyStore.Application.UseCases.Category.Delete;
+
+public class DeleteCategoryUseCase(
+    ICategoryRepository categoryRepository,
+    IUnitOfWork unitOfWork) : IDeleteCategoryUseCase
+{
+    public async Task<Result> ExecuteAsync(int id, CancellationToken ct)
+    {
+        var category = await categoryRepository.GetByIdAsync(id, ct);
+
+        if (category is null)
+            return Result.NotFound(ResourceErrorMessages.CATEGORY_NOT_FOUND);
+
+        categoryRepository.Remove(category);
+        await unitOfWork.CommitAsync(ct);
+
+        return Result.NoContent();
+    }
+}
---
+++ MyStore.Application/UseCases/Category/Delete/IDeleteCategoryUseCase.cs
@@ -0,0 +1,8 @@
+﻿using MyStore.Application.Messages;
+
+namespace MyStore.Application.UseCases.Category.Delete;
+
+public interface IDeleteCategoryUseCase
+{
+    Task<Result> ExecuteAsync(int id, CancellationToken ct);
+}
