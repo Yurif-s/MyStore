368b0031d83506132c3e241226a05ed86dedd99e
Yuri <yurimfs30@gmail.com>
2026-07-02 11:20:58 -0300

    feat(application): add use case for update category

---
+++ MyStore.Application/UseCases/Category/Update/IUpdateCategoryUseCase.cs
@@ -0,0 +1,9 @@
+﻿using MyStore.Application.DTOs.Category;
+using MyStore.Application.Messages;
+
+namespace MyStore.Application.UseCases.Category.Update;
+
+public interface IUpdateCategoryUseCase
+{
+    Task<Result<CategoryDto>> ExecuteAsync(int id, CategoryInputDto dto, CancellationToken ct = default);
+}
---
+++ MyStore.Application/UseCases/Category/Update/UpdateCategoryUseCase.cs
@@ -0,0 +1,33 @@
+﻿using MyStore.Application.DTOs.Category;
+using MyStore.Application.Messages;
+using MyStore.Application.UseCases.Category.Shared;
+using MyStore.Domain.Interfaces;
+
+namespace MyStore.Application.UseCases.Category.Update;
+
+public class UpdateCategoryUseCase(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork) : IUpdateCategoryUseCase
+{
+    public async Task<Result<CategoryDto>> ExecuteAsync(int id, CategoryInputDto dto, CancellationToken ct = default)
+    {
+        var validationResult = await new CategoryValidator().ValidateAsync(dto, ct);
+        if (!validationResult.IsValid)
+            return Result<CategoryDto>.ValidationError(
+                validationResult.Errors.Select(e => e.ErrorMessage));
+
+        var category = await categoryRepository.GetByIdAsync(id);
+
+        if (category is null)
+            return Result<CategoryDto>.NotFound(ResourceErrorMessages.CATEGORY_NOT_FOUND);
+
+        category.Update(dto.Name);
+
+        categoryRepository.Update(category);
+        await unitOfWork.CommitAsync(ct);
+
+        var response = new CategoryDto(
+            category.Name,
+            category.Slug);
+
+        return Result<CategoryDto>.Success(response);
+}
+}
